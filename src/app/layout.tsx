a59ad97e363a6526dac540f6c92af39e7aa980fb
thomas1996 <thomas.kantahck@gmail.com>
2024-10-30 18:11:47 +0100

    remove unusual var

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { getPhotos, getSettings } from "@/sanity/lib/fetch";
+import { getSettings } from "@/sanity/lib/fetch";
 import { urlFor } from "@/sanity/lib/image";
 
 const geistSans = localFont({
