b571a41a644d2c3f930513e6e239b6a569dab13d
thomas1996 <thomas.kantahck@gmail.com>
2024-10-30 14:51:15 +0100

    add .env ?

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,12 +1,8 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-10-29'
 
-// export const dataset = assertValue(
-//   process.env.NEXT_PUBLIC_SANITY_DATASET,
-//   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-// )
 export const dataset = assertValue(
-  process.env.SANITY_STUDIO_DATASET,
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
