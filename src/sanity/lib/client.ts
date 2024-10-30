91bb805bb4b55b82c1631eb5fbb9c0084fcc8ce3
thomas1996 <thomas.kantahck@gmail.com>
2024-10-30 15:03:32 +0100

    resolve env sanity var issue 3

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,9 +1,18 @@
 import { apiVersion, dataset, projectId } from "../env";
 
+// const config = {
+//   projectId,
+//   dataset,
+//   apiVersion,
+//   useCdn:true
+// }
+// export default config
+
 const config = {
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn:true
-}
-export default config
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  useCdn: true,
+  apiVersion: "2023-10-01",
+};
+
+export default config;
