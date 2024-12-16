44caf34f1f1cdb4423f260f72385079b62219a25
thomas1996 <thomas.kanthack@gmail.com>
2024-12-16 11:30:36 +0100

    test

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -7,11 +7,10 @@
  * https://github.com/sanity-io/next-sanity
  */
 
-
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-static'
 
 export { metadata, viewport } from 'next-sanity/studio'
 
