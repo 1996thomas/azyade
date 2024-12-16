0fe5891d270f3138ca53a19064ff090c93178738
thomas1996 <thomas.kanthack@gmail.com>
2024-12-16 10:28:58 +0100

    test without call on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { getPhotos, getProductions } from "@/sanity/lib/fetch";
-import PhotoCards from "./components/PhotoCards";
 import Link from "next/link";
-import ProductionsList from "./components/ProductionsList";
 
 export const revalidate = 60; 
 
 export default async function Home() {
-  const photos = await getPhotos(); 
-  const productions = await getProductions();
+  // const photos = await getPhotos(); 
+  // const productions = await getProductions();
   return (
     <div className="mb-20 w-[90vw] mx-auto">
       <span className="border-b-2 flex border-black w-full h-5 mb-5 my-20" />
@@ -15,7 +13,7 @@ export default async function Home() {
         <h2 className="text-3xl md:text-4xl leading-none">
           RÉALISATIONS
           <span className="opacity-15 ml-1 md:text-3xl text-2xl">
-            ({productions.length})
+            {/* ({productions.length}) */}
           </span>
         </h2>
         <Link className="underline text-sm md:text-2xl" href={"/realisation"}>
@@ -28,7 +26,7 @@ export default async function Home() {
         <h2 className="text-3xl md:text-4xl leading-none">
           PHOTOGRAPHIES
           <span className="opacity-15 ml-1 md:text-3xl text-2xl">
-            ({photos.length})
+            {/* ({photos.length}) */}
           </span>
         </h2>
         <Link className="underline text-sm md:text-2xl" href={"/photos"}>
